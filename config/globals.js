a63d1a1681a31dfe38a24583682e0e0c064bf806
YurikoU <76763897+YurikoU@users.noreply.github.com>
2022-08-10 11:41:11 -0700

    Fixed the GitHub OAuth URL

--- config/globals.js
+++ config/globals.js
@@ -6,7 +6,7 @@ const configurations = {
   'github': {
     'clientId': '9884493d939cb626ce36',
     'clientSecret': '2850f8b1eaaa0c9c563295b6677cd1d2508e855d',
-    'callbackUrl': ' https://delicious-recipe-app.herokuapp.com/github/callback'
+    'callbackUrl': 'https://delicious-recipe-app.herokuapp.com/github/callback'
   },
 }
 
